a7de8486d99326b5225d2017ac98bf275138c5f9
Arun Kumar <java.kumar.arun@gmail.com>
2019-02-04 13:20:20 +0530

    updated coverage file

--- ng-app/src/03-templateVstemplateUrl/app/app.component.ts
+++ ng-app/src/03-templateVstemplateUrl/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
+    // template: `app.component1.html`,
     templateUrl: `app.component.html`,
     styleUrls: [`app.component.css`]
 })
